7bb4c8f1597ba32d9ac54d752df26c807d6a1128
Silvia Lopez <silvia.lopez@nuance.com>
2023-01-13 13:56:28 -0500

    first attempt at select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,38 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+    const displayedValue = getDisplayedValue(value, children);
+    const width = displayedValue.length * 12 + 32; // plus left and right padding
 
-  return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
-  );
+    return (
+        <Wrapper
+            value={value}
+            onChange={onChange}
+            style={{ "--width": width + "px" }}
+        >
+            {children}
+            <Icon id="chevron-down" />
+        </Wrapper>
+    );
 };
+
+const Wrapper = styled.select`
+    background: ${COLORS.transparentGray15};
+    border-radius: 8px;
+    padding: 12px 16px;
+    border: none;
+    color: ${COLORS.gray700};
+    font-size: 1rem;
+    width: var(--width);
+
+    &:hover {
+        color: black;
+    }
+`;
 
 export default Select;

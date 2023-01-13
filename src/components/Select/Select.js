1e82a14216b9cf71e08786da4422bc3c71ed1be5
Silvia Lopez <silvia.lopez@nuance.com>
2023-01-13 14:44:51 -0500

    use custom icon in select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -7,30 +7,56 @@ import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
     const displayedValue = getDisplayedValue(value, children);
-    const width = displayedValue.length * 12 + 32; // plus left and right padding
 
     return (
-        <Wrapper
-            value={value}
-            onChange={onChange}
-            style={{ "--width": width + "px" }}
-        >
-            {children}
-            <Icon id="chevron-down" />
+        <Wrapper>
+            <NativeSelect value={value} onChange={onChange}>
+                {children}
+            </NativeSelect>
+            <PresentationalBit>
+                {displayedValue}
+                <Icon id="chevron-down" strokeWidth={1} size={24} />
+            </PresentationalBit>
         </Wrapper>
     );
 };
 
-const Wrapper = styled.select`
+const Wrapper = styled.div`
+    position: relative;
+    width: max-content;
+`;
+
+const NativeSelect = styled.select`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    /* fix for safari to override height */
+    appearance: none;
+`;
+
+const PresentationalBit = styled.div`
     background: ${COLORS.transparentGray15};
     border-radius: 8px;
     padding: 12px 16px;
-    border: none;
+    padding-right: 10px;
     color: ${COLORS.gray700};
-    font-size: 1rem;
-    width: var(--width);
+    font-size: ${16 / 16}rem; //1rem;
 
-    &:hover {
+    /* position icon */
+    display: flex;
+    align-items: center;
+    gap: 24px;
+
+    /* the adjacent sibling combinator */
+    ${NativeSelect}:focus + & {
+        // TODO: add styles for other browsers
+        outline: 5px auto -webkit-focus-ring-color;
+    }
+
+    ${NativeSelect}:hover + & {
         color: black;
     }
 `;
